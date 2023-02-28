dd28f71799ca9f166141630638a947132b111402
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2023-02-28 22:44:56 +1100

    game-genre: update genre repo

--- src/main/java/com/avengers/gamera/repository/GenreRepository.java
+++ src/main/java/com/avengers/gamera/repository/GenreRepository.java
@@ -3,13 +3,5 @@ package com.avengers.gamera.repository;
 import com.avengers.gamera.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
-
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-
-    Boolean existsGenresByName(String name);
-
-    @Override
-    List<Genre> findAllById(Iterable<Long> longs);
 }
