49b0cdec58a1078251501133620f47aa5aa4ab4d
Nancy Lu <nancylyn98@gmail.com>
2023-03-06 02:05:06 +1030

    Fix Article Post

---
+++ src/main/java/com/avengers/gamera/dto/game/GameSlimGetDto.java
@@ -0,0 +1,15 @@
+package com.avengers.gamera.dto.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameSlimGetDto {
+    private Long id;
+    private String name;
+}
