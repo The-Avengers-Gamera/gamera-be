a70d1a825c719224052aa39d2de44c5f4fbbd8d9
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2023-03-08 16:47:31 +1100

    GF-256:fix Game genreList getDto

--- src/main/java/com/avengers/gamera/mapper/GameMapper.java
+++ src/main/java/com/avengers/gamera/mapper/GameMapper.java
@@ -5,24 +5,12 @@ import com.avengers.gamera.dto.game.GamePostDto;
 import com.avengers.gamera.dto.game.GameUpdateDto;
 import com.avengers.gamera.entity.Game;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface GameMapper {
 
     GameGetDto GameToGameGetDto(Game game);
-
-
-    @Mapping(source = "name",target = "name")
-    @Mapping(source = "platform",target = "platform")
-    @Mapping(source = "releaseDate",target = "releaseDate")
-    @Mapping(source = "country",target = "country")
-    @Mapping(source = "scores",target = "scores")
-    @Mapping(source = "developers",target = "developers")
-    @Mapping(source = "publishers",target = "publishers")
-    @Mapping(source = "introduction",target = "introduction")
-    @Mapping(source = "description",target = "description")
     Game GamePostDtoToGame(GamePostDto gamePostDto);
 
     Game GameUpdateDtoToGame(GameUpdateDto gameUpdateDto);
