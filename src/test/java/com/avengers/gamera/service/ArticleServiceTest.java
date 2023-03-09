eba658a72e47fbc78841b268124f662ab4ee8420
Xiangcheng <93109500+ShaunChang@users.noreply.github.com>
2023-03-10 08:47:14 +1030

    GF 308 article u test (#56)

--- src/test/java/com/avengers/gamera/service/ArticleServiceTest.java
+++ src/test/java/com/avengers/gamera/service/ArticleServiceTest.java
@@ -1,24 +1,27 @@
 package com.avengers.gamera.service;
 
-
 import com.avengers.gamera.constant.EArticleType;
 import com.avengers.gamera.dto.article.ArticleGetDto;
-import com.avengers.gamera.dto.article.ArticlePostDto;
 import com.avengers.gamera.entity.Article;
+import com.avengers.gamera.mapper.ArticleMapper;
+import com.avengers.gamera.repository.ArticleRepository;
+import com.avengers.gamera.utils.MockArticleData;
+
+import com.avengers.gamera.dto.article.ArticlePostDto;
 import com.avengers.gamera.entity.Game;
 import com.avengers.gamera.entity.Genre;
 import com.avengers.gamera.entity.User;
-import com.avengers.gamera.mapper.ArticleMapper;
-import com.avengers.gamera.repository.ArticleRepository;
-import com.avengers.gamera.repository.GameRepository;
-import com.avengers.gamera.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -29,13 +32,11 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ArticleServiceTest {
-
-    @Mock
-    private ArticleMapper articleMapper;
-
     @Mock
     private ArticleRepository articleRepository;
 
+    @Mock
+    private ArticleMapper articleMapper;
 
     @Mock
     private GameService gameService;
@@ -99,4 +100,46 @@ class ArticleServiceTest {
         assertEquals(articleGetDto, mockArticleGetDto);
         verify(articleRepository).save(mockArticle);
     }
+    ArticleGetDto ExpectUpdatedArticleGetDto = ArticleGetDto.builder().id(MockArticleData.articleId)
+            .game(null)
+            .user(null)
+            .commentList(new ArrayList<>())
+            .coverImgUrl("url")
+            .title("update title")
+            .text("update text")
+            .type(EArticleType.REVIEW)
+            .createdTime(OffsetDateTime.now())
+            .updatedTime(OffsetDateTime.now()).build();
+
+    private ArticleGetDto generateArticleGetDto(Article article){
+        return  ArticleGetDto.builder().id(MockArticleData.articleId)
+                .game(null)
+                .user(null)
+                .commentList(new ArrayList<>())
+                .coverImgUrl(article.getCoverImgUrl())
+                .title(article.getTitle())
+                .text(article.getText())
+                .type(EArticleType.REVIEW)
+                .createdTime(article.getCreatedTime())
+                .updatedTime(OffsetDateTime.now()).build();
+    }
+
+    @Test
+    void updateArticleTest() {
+        when(articleRepository.findById(MockArticleData.articleId)).thenReturn(Optional.ofNullable(MockArticleData.mockArticle));
+        when(articleRepository.save(any())).thenReturn(MockArticleData.mockArticle);
+        when(articleMapper.articleToArticleGetDto(MockArticleData.mockArticle)).thenAnswer(new Answer<ArticleGetDto>() {
+            @Override
+            public ArticleGetDto answer(InvocationOnMock invocation) throws Throwable {
+                Article article = (Article) invocation.getArguments()[0];
+                return generateArticleGetDto (article);
+            }
+        });
+        ArticleGetDto updatedArticleGetDto = articleService.updateArticle(MockArticleData.mockArticlePutDto, MockArticleData.articleId);
+
+        assertEquals(ExpectUpdatedArticleGetDto.getText(), updatedArticleGetDto.getText());
+        assertEquals(ExpectUpdatedArticleGetDto.getTitle(), updatedArticleGetDto.getTitle());
+    }
+
+
 }
