25db3efb6443157fed6e039590648122d93434e5
Lucas HU <986117324@qq.com>
2023-03-11 20:01:17 +1100

    feat: fix

--- src/main/resources/db/migration/V13__add_column_to_game_table_and_article_table.sql
+++ src/main/resources/db/migration/V13__add_column_to_game_table_and_article_table.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "article" ADD subtitle VARCHAR(255) NOT NULL;
-ALTER TABLE "game" ADD img_url VARCHAR(255);
+ALTER TABLE "article" ADD subtitle VARCHAR(255);
+ALTER TABLE "game" ADD img_url VARCHAR(255) NOT NULL;
