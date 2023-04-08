e1182f7a1d87eefded611ed52f4d52bee5d1bd2e
nancylyn98 <68838775+nancylyn98@users.noreply.github.com>
2023-04-08 17:46:06 +0930

    GF 493 User-View-More-Login-authentication-backend (#91)
    * add auth
    
    * pause chat gpt create article scheduled task
    
    ---------
    
    Co-authored-by: Weite Dai <weitedai.dev@gmail.com>

--- src/main/java/com/avengers/gamera/util/ChatGptScheduledTask.java
+++ src/main/java/com/avengers/gamera/util/ChatGptScheduledTask.java
@@ -3,7 +3,6 @@ package com.avengers.gamera.util;
 import com.avengers.gamera.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +11,8 @@ import org.springframework.stereotype.Component;
 public class ChatGptScheduledTask {
     private final ArticleService articleService;
 
-    @Scheduled(fixedDelay = 1000 * 60 * 10)
+    // TODO: replace new openai api token
+    //    @Scheduled(fixedDelay = 1000 * 60 * 10)
     public void scheduledTask() {
         log.info("Scheduled task create review by chatgpt is running");
         try {
